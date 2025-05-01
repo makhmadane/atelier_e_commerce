ab8121d5aadaab398ccff6f65cf0e1014e240f90
makhmadane96 <makhmadane.lo@jmadsolutions.sn>
2025-05-01 18:22:39 +0100

    genreate facture

--- index.js
+++ index.js
@@ -7,15 +7,61 @@ let tabProduits = [
 ];
 let tabPanier = [];
 let contenuPanier = document.getElementById("contenuPanier");
+let contenuFactureProduit = document.getElementById("contenuFactureProduit");
 let article = document.getElementById("articles");
 displayProducts(tabProduits);
 
 
+function generatePDF(){
+    let element = document.getElementById('infoPdf');
+
+    var opt = {
+      margin:       1,
+      filename:     'facture.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+     
+    html2pdf().from(element).set(opt).save();
+}
+function printFacture(){
+    let nom = document.getElementById('nom_complet').value;
+    let age = document.getElementById('age').value;
+    let email = document.getElementById('email').value;
+    let tel = document.getElementById('tel').value;
+
+    document.getElementById('infoClient').innerHTML = 
+    `
+        <tr>
+            <td>${nom}</td>
+            <td>${age}</td>
+            <td>${email}</td>
+            <td>${tel}</td>
+        </tr>
+    `;
+    contenuFactureProduit.innerHTML ="";
+    let somme = 0;
+    tabPanier.forEach((p,i) => {
+        somme += p.prix * p.quantite;
+        var ligne = `<tr>
+                        <td>${p.libelle}</td>
+                        <td>${p.quantite}</td>
+                        <td>${p.prix * p.quantite}</td>
+                    </tr>`
+    contenuFactureProduit.innerHTML += ligne;
+    });
+    document.getElementById('totalFacture').innerText = somme + "FCFA";
+}
+
 function deleteProduct(id){
+    let produit = tabProduits.find(p => p.id == id);
+    produit.stock = produit.stock + tabPanier.filter(p => p.id == id)[0].quantite;
 
     tabPanier = tabPanier.filter(p => p.id != id);
     
     displayPanier();
+    displayProducts(tabProduits);
 
 }
 
@@ -44,21 +90,26 @@ function ajoutPanier(id){
 function displayPanier(){
     contenuPanier.innerHTML = "";
     let somme = 0;
-    tabPanier.forEach((p,i) => {
-        somme += p.prix * p.quantite;
-        var ligne = `<tr>
-                        <td><img class="card-img-top" src="${p.photo}" alt="Card image cap"></td>
-                        <td>${p.libelle}</td>
-                        <td>${p.quantite}</td>
-                        <td>${p.prix}</td>
-                        <td>${p.prix * p.quantite}</td>
-                        <td>
-                            <button class="btn btn-secondary" onclick="deleteProduct('${p.id}')"> Delete </button>
-                        </td>
-                    </tr>`
-    contenuPanier.innerHTML += ligne;
+    if(tabPanier.length === 0){
+        contenuPanier.innerHTML = "Votre panier est vide."
+    }else{
+        tabPanier.forEach((p,i) => {
+            somme += p.prix * p.quantite;
+            var ligne = `<tr>
+                            <td><img class="card-img-top" src="${p.photo}" alt="Card image cap"></td>
+                            <td>${p.libelle}</td>
+                            <td>${p.quantite}</td>
+                            <td>${p.prix}</td>
+                            <td>${p.prix * p.quantite}</td>
+                            <td>
+                                <button class="btn btn-secondary" onclick="deleteProduct('${p.id}')"> Delete </button>
+                            </td>
+                        </tr>`
+        contenuPanier.innerHTML += ligne;
+        });
+    }
+  
     document.getElementById('total').innerText = somme + "FCFA";
-    });
 
 }
 
